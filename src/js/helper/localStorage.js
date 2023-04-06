bfcfc503c937399af0b1425e5b47e1aad383e6a9
Manh Quan <manhquan.05012002@gmail.com>
2023-04-06 17:21:37 +0700

    fix issues rename file

--- src/js/helper/localStorage.js
+++ src/js/helper/localStorage.js
@@ -1,6 +1,7 @@
 const getLocalStorage = (key) => JSON.parse(localStorage.getItem(key)) || null;
 
-const setLocalStorage = (key, data) => localStorage.setItem(key, JSON.stringify(data));
+const setLocalStorage = (key, data) =>
+  localStorage.setItem(key, JSON.stringify(data));
 
 const clearLocalStorage = (key) => localStorage.removeItem(key);
 
